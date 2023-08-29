12ca284fe7a35a8600fe1e50573fe5e3c9da0127
alexandre-assad <alexandre.assad@laplateforme.io>
2023-08-29 10:22:42 +0700

    [ADD] Echequier_test.py methods

--- Test/Materials_test/Echequier_test.py
+++ Test/Materials_test/Echequier_test.py
@@ -5,3 +5,19 @@ class TestEchequier(unittest.TestCase):
     
     def test_setup(self):
         pass
+    
+    
+    def test_display(self):
+        pass
+    
+    
+    def test_evaluate(self):
+        pass
+    
+    
+    def test_play_move(self):
+        pass
+    
+    
+    def test_promote_pion(self):
+        pass
