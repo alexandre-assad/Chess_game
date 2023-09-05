86ccbacc7f91bbc3b3f4cf927a503e18d9e8023c
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-05 09:38:13 +0200

    [ADD] Fou generates moves() method

--- Materials/Fou.py
+++ Materials/Fou.py
@@ -8,9 +8,55 @@ class Fou(Piece):
         self.letter = "F"
 
 
-    def generate_moves(self) -> list:
+    def generate_moves(self,echequier) -> list:
         """
         Input : self
         Output : list de coups (list de [Objet pièce, postion d'arrivée])
         """
-        pass
+        piece = echequier.board[self.li-1][self.col-1]
+        moves = []
+        run = True
+        pos = [self.li-1,self.col-1]
+        while run:
+            if pos[0] != 7 and pos[1] != 7:
+                if echequier.board[pos[0]+1][pos[1]+1] == " ":
+                    moves.append([self,pos[0]+1+1,pos[1]+1+1])
+                    pos = [pos[0]+1,pos[1]+1]
+                else:
+                    run = False
+            else:
+                run = False
+        run = True
+        pos = [self.li-1,self.col-1]
+        while run:
+            if pos[0] != 7 and pos[1] != 0:
+                if echequier.board[pos[0]+1][pos[1]-1] == " ":
+                    moves.append([piece,pos[0]+1+1,pos[1]-1+1])
+                    pos = [pos[0]+1,pos[1]-1]
+                else:
+                    run = False
+            else:
+                run = False
+        run = True
+        pos = [self.li-1,self.col-1]
+        while run:
+            if pos[0] != 0 and pos[1] != 0:
+                if echequier.board[pos[0]-1][pos[1]-1] == " ":
+                    moves.append([self,pos[0]-1+1,pos[1]-1+1])
+                    pos = [pos[0]-1,pos[1]-1]
+                else:
+                    run = False
+            else:
+                run = False
+        run = True
+        pos = [self.li-1,self.col-1]
+        while run:
+            if pos[0] != 0 and pos[1] != 7:
+                if echequier.board[pos[0]-1][pos[1]+1] == " ":
+                    moves.append([self,pos[0]-1+1,pos[1]+1+1])
+                    pos = [pos[0]-1,pos[1]+1]
+                else:
+                    run = False
+            else:
+                run = False
+        return moves
--- Test/Materials/Fou_test.py
+++ Test/Materials/Fou_test.py
@@ -8,4 +8,4 @@ class TestFou(unittest.TestCase):
     def test_generate_moves(self):
         Echequier_1 = Echequier()
         Echequier_1.play_move(Coup(Echequier_1.board[1][1],3,2))
-        self.assertEqual(Echequier.board[0][2].generate_moves(),[[Fou("w",1,3),2,2],[Fou("w",1,3),3,1]])
+        self.assertEqual(Echequier_1.board[0][2].generate_moves(Echequier_1),[[Echequier_1.board[0][2],2,2],[Echequier_1.board[0][2],3,1]])
