6a2e7158cec5d3d499eec123c39e009708fface3
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-05 11:47:06 +0200

    [ADD] Pion test generate_moves()

---
+++ Test/Materials/Pion_test.py
@@ -0,0 +1,16 @@
+import unittest
+from Materials.Echequier import Echequier
+from Materials.Pion import Pion
+from Coup import Coup
+
+class TestTour(unittest.TestCase):
+
+    def test_generate_moves(self):
+        Echequier_1 = Echequier()
+        Piona2 = Echequier_1.board[1][0]
+        self.assertEqual(Piona2.generate_moves(Echequier_1),[[Piona2,3,1],[Piona2,4,1]])
+        Echequier_1.board[4][0] = Pion("b",5,1)
+        Echequier_1.play_move(Coup(Piona2,4,1))
+        self.assertEqual(Piona2.generate_moves(Echequier_1),[])
+        Echequier_1.board[4][1] = Pion("b",5,2)
+        self.assertEqual(Piona2.generate_moves(Echequier_1),[Piona2,5,2])
