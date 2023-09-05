7bb8a44c215662adf53ec454c35cdf9a0638e3f0
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-05 16:07:33 +0200

    [ADD] Dame generate_moves() method

--- Materials/Dame.py
+++ Materials/Dame.py
@@ -1,4 +1,6 @@
 from Materials.Piece import Piece
+from Materials.Tour import Tour
+from Materials.Fou import Fou
 
 class Dame(Piece):
     
@@ -6,3 +8,10 @@ class Dame(Piece):
         super().__init__(color,li,col)
         self.point = 9
         self.letter ="D"
+
+    def generate_moves(self, echequier) -> list:
+        moves = Tour(self.color,self.li,self.col).generate_moves(echequier) + Fou(self.color,self.li,self.col).generate_moves(echequier)
+        for i in range(len(moves)):
+            moves[i][0] = self
+        
+        return moves
