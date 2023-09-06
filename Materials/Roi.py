535476c35098a7d6fc90cff868c0140fc5d33ed9
Alexandre Assad <alexandre.assad@laplateforme.io>
2023-09-06 15:11:16 +0200

    [FIX] Roi movement issues

--- Materials/Roi.py
+++ Materials/Roi.py
@@ -13,8 +13,9 @@ class Roi(Piece):
         pos = [self.li-1,self.col-1]
         for i in range(-1,2,1):
             for j in range(-1,2,1):
-                if echequier.board[pos[0]+i][pos[1]+j] == " ":
-                    moves.append([piece,pos[0]+i+1,pos[1]+j+1])
-                elif echequier.board[pos[0]+i][pos[1]+j] != " " and echequier.board[pos[0]+i][pos[1]+j].color != piece.color and echequier.board[pos[0]+i][pos[1]+j].is_defended == False:
-                     moves.append([piece,pos[0]+i+1,pos[1]+j+1])
+                if pos[0]+i <= 7 and pos[0]+i >= 0 and pos[1]+j <= 7 and pos[1]+j >= 0:
+                    if echequier.board[pos[0]+i][pos[1]+j] == " ":
+                        moves.append([piece,pos[0]+i+1,pos[1]+j+1])
+                    elif echequier.board[pos[0]+i][pos[1]+j] != " " and echequier.board[pos[0]+i][pos[1]+j].color != piece.color and echequier.board[pos[0]+i][pos[1]+j].is_defended == False:
+                        moves.append([piece,pos[0]+i+1,pos[1]+j+1])
         return moves
